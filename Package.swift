64f964763b4e10e5a330475d37e32e1c9fa05223
ANUAR <anuarprogramm@gmail.com>
2022-09-28 22:31:29 +0600

    setup package.swift

--- Package.swift
+++ Package.swift
@@ -6,20 +6,18 @@ import PackageDescription
 
 let package = Package(
     name: "socketSrver",
-//    dependencies: [
-//        .package(url: "https://github.com/Kitura/BlueSocket.git", from:"1.0.8"),
-//    ],
     dependencies: [
-            .package(path: "Libs/BlueSocket")
-        ],
+        .package(url: "https://github.com/Kitura/BlueSocket.git", from:"1.0.8")
+    ],
     targets: [
         .executableTarget(
             name: "socketSrver",
             dependencies: [.product(name: "Socket", package: "BlueSocket")],
-                        resources: [.process("Resources")]
-        ),
+            resources: [.process("Resources")]
+            ),
         .testTarget(
             name: "socketSrverTests",
             dependencies: ["socketSrver"]),
     ]
 )
+
